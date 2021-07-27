f3c6ce2ad7d9e017f387c1e0e71f401419ccff1a
xLinc x <LCheng2413@yahoo.com>
2021-07-26 21:37:35 -0400

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -13,14 +13,7 @@ CREATE TABLE employees (
      gender VARCHAR NOT NULL,
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
-); 
-CREATE TABLE Dept_Emp (
-	 dept_no INT NOT NULL,
-     emp_no INT NOT NULL,
-     from_date DATE NOT NULL,
-     to_date DATE NOT NULL,
-     PRIMARY KEY (dept_no, emp_no)
-); 
+);
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -29,7 +22,7 @@ dept_no VARCHAR(4) NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
-);
+); 
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -38,13 +31,20 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
 CREATE TABLE Titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-
-SELECT * FROM departments;
+CREATE TABLE Dept_Emp (
+	 emp_no INT NOT NULL, 
+	 dept_no VARCHAR NOT NULL,
+   from_date DATE NOT NULL,
+   to_date DATE NOT NULL,
+   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+   PRIMARY KEY (dept_no, emp_no)
+); 
