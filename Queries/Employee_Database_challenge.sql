fb09bff9376214697276a9ce3ddd301c42e653c5
Lin <83035801+RaoxLinc@users.noreply.github.com>
2021-07-31 22:08:12 -0400

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -15,7 +15,7 @@ ORDER BY emp_no ASC;
 SELECT * FROM retirement_titles;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+SELECT DISTINCT ON (rt.emp_nSo) rt.emp_no,
 rt.first_name,
 rt.last_name,
 rt.title
@@ -32,3 +32,23 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
+
+-- Deliverable part 2
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentor_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+SELECT * FROM mentor_eligibility
